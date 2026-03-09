a7202cb1dd7a6d64e36fbf8e969b787ca3d6f718
eniompw <71254710+eniompw@users.noreply.github.com>
2026-03-09 14:43:37 +0000

    Implement password authentication in home route and update form in index.html

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    q = request.args.get("q")
-    print(q)
+    if request.method == "POST":
+        pw = request.form['pw']
+        if pw == "123":
+            return "Access granted"
+        else:
+            return "Access denied"
     return render_template("index.html")
