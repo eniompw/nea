d0c72bf5df87fbd2597eba63e593f2a1f65c6d5f
eniompw <71254710+eniompw@users.noreply.github.com>
2026-03-13 14:56:01 +0000

    Implement signup functionality and update login form

--- app.py
+++ app.py
@@ -2,15 +2,22 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return "Signup page"
+    if request.method == "POST":
+        f = open("login.txt", "w")
+        f.write(request.form['password'])
+        f.close()
+    return render_template("signup.html")
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        pw = request.form['pw']
-        if pw == "123":
+        pw = request.form['password']
+        f = open("login.txt", "r")
+        stored_password = f.read()
+        f.close()
+        if pw == stored_password:
             return "Access granted"
         else:
             return "Access denied"
